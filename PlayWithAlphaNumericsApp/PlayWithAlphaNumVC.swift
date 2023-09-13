1a4a0ffaa115f324629d83962cc49e51732f1639
Sai Prashanth Volete <s559234@nwmissouri.edu>
2023-09-13 12:28:38 -0700

    Modified buttons and switches

--- PlayWithAlphaNumericsApp/PlayWithAlphaNumVC.swift
+++ PlayWithAlphaNumericsApp/PlayWithAlphaNumVC.swift
@@ -12,69 +12,89 @@ class PlayWithAlphaNumVC: UIViewController {
     
     @IBOutlet weak var headerLBL: UILabel!
     
-    
     @IBOutlet weak var firstNumLBL: UILabel!
     @IBOutlet weak var secondNumLBL: UILabel!
-    
     
     @IBOutlet weak var firstStrTF: UITextField!
     @IBOutlet weak var secondStrTF: UITextField!
     
-    @IBOutlet weak var numberSWCH: UISwitch!
-   
-    
-    @IBOutlet weak var stringSWCH: UISwitch!
-    
-    @IBOutlet weak var firstNumStepper: UIStepper!
-    
-    @IBOutlet weak var secondNumStepper: UIStepper!
     
     
+    
+    @IBAction func numberSWCH(_ sender: Any) {
+        self.numbersStepper[0].isEnabled = true
+        self.numbersStepper[1].isEnabled = true
+        stringAndPatternBTN[0].isEnabled=true
+        stringAndPatternBTN[1].isEnabled=false
+        self.optionSWCH[1].isOn=false
+        
+    }
+    
+    @IBOutlet var optionSWCH: [UISwitch]!
+    
+    
+    
+    
+    @IBAction func firstNumStepper(_ sender: UIStepper) {
+        
+    }
+    
+    @IBAction func secondnumStepper(_ sender: UIStepper) {
+    }
+    
+    @IBOutlet var numbersStepper: [UIStepper]!
+    
+    
+    
+    //message text view
     @IBOutlet weak var messageTV: UITextView!
     
+
+    @IBAction func generatePatterns(_ sender: UIButton) {
+    }
+    
+   
+    
+    @IBAction func manipulateStrings(_ sender: UIButton) {
+    }
     
     
-    @IBOutlet weak var generatePattern: UIButton!
-    @IBOutlet weak var manipulateStrings: UIButton!
-    
-    
-    @IBOutlet weak var onReset: UIButton!
+    @IBOutlet var stringAndPatternBTN: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.headerLBL.text=String(format:"%@\nPlaywithAlphaNumerics™️","\u{0c38}\u{0c4d}\u{0c35}\u{0c3e}\u{0c17}\u{0c24}\u{0c02}")
         
-        firstNumStepper.isEnabled=false
-        secondNumStepper.isEnabled=false
+        self.numbersStepper[0].isEnabled = false
+        self.numbersStepper[1].isEnabled = false
+        
         firstStrTF.isEnabled=false
         secondStrTF.isEnabled=false
-        generatePattern.isEnabled=false
-        manipulateStrings.isEnabled=false
+        
+        stringAndPatternBTN[0].isEnabled=false
+        stringAndPatternBTN[1].isEnabled=false
         
         messageTV.text=""
     }
     
-    @IBAction func numberSWCH(_ sender: UISwitch){
-        firstNumStepper.isEnabled=true
-        secondNumStepper.isEnabled=true
-        generatePattern.isEnabled=true
-    }
+  
+   
     
     
-    @IBAction func stringSWCH(_ sender: UISwitch){
-        
+    @IBAction func stringswitch(_ sender: UISwitch) {
         firstStrTF.isEnabled=true
         secondStrTF.isEnabled=true
-        manipulateStrings.isEnabled=true
+        stringAndPatternBTN[0].isEnabled=false
+        stringAndPatternBTN[1].isEnabled=true
+        self.optionSWCH[1].isOn=true
     }
+    
+    
 
     @IBAction func generatePattern(_ sender: UIButton) {
         
     }
     
-    @IBAction func manipulateStrings(_ sender: UIButton){
-        
-    }
-  
+   
 }
